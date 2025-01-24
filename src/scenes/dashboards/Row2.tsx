698e884ed35b5b2cbf17e0129fb3da4cbd155086
abhishek-chiluka99 <chilukaabhishek9@gmail.com>
2025-01-24 18:58:47 +1100

    structured with basic server and client code.

---
+++ src/scenes/dashboards/Row2.tsx
@@ -0,0 +1,16 @@
+import DashboardBox from "../../components/DashboardBox"
+
+
+
+
+const Row2 = () => {
+    return (
+        <>
+            <DashboardBox bgcolor="#fff" gridArea="d"></DashboardBox>
+            <DashboardBox bgcolor="#fff" gridArea="e"></DashboardBox>
+            <DashboardBox bgcolor="#fff" gridArea="f"></DashboardBox>
+        </>
+    )
+}
+
+export default Row2
